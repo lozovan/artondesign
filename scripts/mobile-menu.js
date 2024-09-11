7819e08cbd70d808979b584614c22aff1e550758
Vitalii Lozovan <vitalulozovan@gmail.com>
2024-09-11 17:08:09 +0000

    add about change make

--- scripts/mobile-menu.js
+++ scripts/mobile-menu.js
@@ -5,13 +5,11 @@ const socialIcons = document.querySelector('.header__social');
 menuToggle.addEventListener('click', () => {
     menuToggle.classList.toggle('open');
     navMenu.classList.toggle('open');
-    socialIcons.classList.toggle('open');
 });
 
 document.addEventListener('click', (event) => {
     if (!menuToggle.contains(event.target) && !navMenu.contains(event.target) && !socialIcons.contains(event.target)) {
         menuToggle.classList.remove('open');
         navMenu.classList.remove('open');
-        socialIcons.classList.remove('open');
     }
 });
