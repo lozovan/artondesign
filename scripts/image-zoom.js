47f5b3e170b33dc5169ed14e21d4ea62446b45ce
Vitalii Lozovan <vitalulozovan@gmail.com>
2025-01-03 16:52:18 +0000

    add new fuctional info project details

--- scripts/image-zoom.js
+++ scripts/image-zoom.js
@@ -1,10 +1,40 @@
-const images = document.querySelectorAll('img');
+const gridImages = document.querySelectorAll('.grid-gallery img');
+  const lightbox = document.querySelector('.lightbox');
+  const lightboxImg = lightbox.querySelector('img');
+  const prevBtn = lightbox.querySelector('.prev');
+  const nextBtn = lightbox.querySelector('.next');
+  const closeBtn = lightbox.querySelector('.close');
 
-images.forEach(image => {
-  image.addEventListener('mouseover', () => {
-    image.style.transform = 'scale(1.1)';
+  let currentIndex = 0;
+
+  function showLightbox(index) {
+    currentIndex = index;
+    lightboxImg.src = gridImages[currentIndex].src;
+    lightbox.style.display = 'flex';
+  }
+
+  function hideLightbox() {
+    lightbox.style.display = 'none';
+  }
+
+  function showPrev() {
+    currentIndex = (currentIndex - 1 + gridImages.length) % gridImages.length;
+    lightboxImg.src = gridImages[currentIndex].src;
+  }
+
+  function showNext() {
+    currentIndex = (currentIndex + 1) % gridImages.length;
+    lightboxImg.src = gridImages[currentIndex].src;
+  }
+
+  gridImages.forEach((img, index) => {
+    img.addEventListener('click', () => showLightbox(index));
   });
-  image.addEventListener('mouseout', () => {
-    image.style.transform = 'scale(1)';
+
+  closeBtn.addEventListener('click', hideLightbox);
+  prevBtn.addEventListener('click', showPrev);
+  nextBtn.addEventListener('click', showNext);
+
+  lightbox.addEventListener('click', (e) => {
+    if (e.target === lightbox) hideLightbox();
   });
-});
